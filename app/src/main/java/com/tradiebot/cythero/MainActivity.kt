547b3425732958836abec227f246e79292d110e7
Deltashot <dimitar.hristijan.najdovski@gmail.com>
2022-11-23 01:03:54 +0100

    attempt to make request....

--- app/src/main/java/com/tradiebot/cythero/MainActivity.kt
+++ app/src/main/java/com/tradiebot/cythero/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tradiebot.cythero
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +12,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tradiebot.cythero.ui.theme.CytheroTheme
+import logcat.logcat
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val test: Activity = this
+
+        Thread(Runnable {
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                val client = OkHttpClient().newBuilder()
+                    .build()
+                // val mediaType = "text/plain".toMediaTypeOrNull()
+                val body: RequestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
+                    // .setType(mediaType!!)
+                    .addFormDataPart("email", "dimitar.najdovski.example@gmail.com")
+                    .addFormDataPart("password", "Dimitar123")
+                    .addFormDataPart("from_web", "true")
+                    .build()
+                val request: Request = Request.Builder()
+                    .url("https://api.cythero.com/api/user/auth/login")
+                    .method("POST", body)
+                    .build()
+                val response = client.newCall(request).execute()
+                logcat { "$response test $response" }
+            })
+        }).start()
+
+        TestAuth().test(test)
+
+
         setContent {
             CytheroTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +57,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
     }
 }
 
