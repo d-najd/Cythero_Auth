3b1476fbbbed14b46582a5965975e0f9856827bf
Deltashot <dimitar.hristijan.najdovski@gmail.com>
2022-11-23 21:01:17 +0100

    backup

--- app/src/main/java/com/tradiebot/cythero/app/ui/login/LoginPresenter.kt
+++ app/src/main/java/com/tradiebot/cythero/app/ui/login/LoginPresenter.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.tradiebot.cythero.app.ui.base.presenter.BasePresenter
 import com.tradiebot.cythero.domain.user.interactor.LoginUser
 import com.tradiebot.cythero.domain.user.model.UserLoginUpdate
+import com.tradiebot.cythero.domain.user.service.UserService
 import com.tradiebot.cythero.presentation.login.LoginState
 import com.tradiebot.cythero.presentation.login.LoginStateImpl
 import com.tradiebot.cythero.util.launchIO
@@ -17,7 +18,9 @@ private data class User(val userData: String)
 
 class LoginPresenter(
     private val state: LoginStateImpl = LoginState() as LoginStateImpl,
-    private val loginUser: LoginUser = Injekt.get(),
+
+    //private val test: UserService = Injekt.get()
+    //private val loginUser: LoginUser = Injekt.get(),
     ) : BasePresenter<LoginController>(), LoginState by state {
 
 
--- app/src/main/java/com/tradiebot/cythero/domain/user/interactor/LoginUser.kt
+++ app/src/main/java/com/tradiebot/cythero/domain/user/interactor/LoginUser.kt
@@ -2,10 +2,11 @@ package com.tradiebot.cythero.domain.user.interactor
 
 import com.tradiebot.cythero.domain.user.model.UserComplete
 import com.tradiebot.cythero.domain.user.model.UserLoginUpdate
+import com.tradiebot.cythero.domain.user.service.UserService
 import com.tradiebot.cythero.service.user.UserServiceImpl
 
 class LoginUser(
-    private val userService: UserServiceImpl,
+    private val userService: UserService,
 ) {
 
     suspend fun await(user: UserLoginUpdate): UserComplete {
