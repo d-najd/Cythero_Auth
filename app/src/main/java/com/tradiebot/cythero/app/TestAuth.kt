6330ba9fe5fbdb6a7cba59dc2ab6517b498b4fe6
Deltashot <dimitar.hristijan.najdovski@gmail.com>
2022-11-23 23:30:50 +0100

    cleanup

--- app/src/main/java/com/tradiebot/cythero/app/TestAuth.kt
+++ app/src/main/java/com/tradiebot/cythero/app/TestAuth.kt
@@ -37,52 +37,5 @@ class TestAuth{
                 }
             }
         })
-        /*
-        val request = Request.Builder()
-            .url("http://publicobject.com/helloworld.txt")
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                    for ((name, value) in response.headers) {
-                        println("$name: $value")
-                    }
-
-                    println(response.body!!.string())
-                }
-            }
-        })
-         */
     }
-
-
-    /*
-
-            Thread(Runnable {
-            this@MainActivity.runOnUiThread(java.lang.Runnable {
-                val client = OkHttpClient().newBuilder()
-                    .build()
-                // val mediaType = "text/plain".toMediaTypeOrNull()
-                val body: RequestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
-                    // .setType(mediaType!!)
-                    .addFormDataPart("email", "dimitar.najdovski.example@gmail.com")
-                    .addFormDataPart("password", "Dimitar123")
-                    .addFormDataPart("from_web", "true")
-                    .build()
-                val request: Request = Request.Builder()
-                    .url("https://api.cythero.com/api/user/auth/login")
-                    .method("POST", body)
-                    .build()
-                val response = client.newCall(request).execute()
-                logcat { "$response test $response" }
-            })
-        }).start()
-     */
 }
